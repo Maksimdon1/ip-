e7a736e226a4292908611e44fd307c0589568595
Maksimdon1 <111438708+Maksimdon1@users.noreply.github.com>
2023-08-15 19:50:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
+    console.log(req.socket.remoteAddress)
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
