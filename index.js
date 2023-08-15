ca1578d0254bea777fce6511f0c546e431ebac47
Maksimdon1 <111438708+Maksimdon1@users.noreply.github.com>
2023-08-15 19:55:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    console.log(req.socket.remoteAddress)
+    
     console.log(req.headers['x-forwarded-for'] )
     res.send('соси хуй дарий  '+ req.headers['x-forwarded-for'])
 })
