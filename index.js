952006b4f93604e2a06e64a241f2e744413ea6db
Maksimdon1 <111438708+Maksimdon1@users.noreply.github.com>
2023-08-15 20:10:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,40 @@ app.get('/name', (req, res) => {
     console.log(req.headers['x-forwarded-for'] )
     res.send('соси хуй дарий  '+ req.headers['x-forwarded-for'])
 })
+
+app.put("/server/light", function(req, res) {
+
+
+    if(     req.body.id && req.body.value   ){
+        const {id, value} = req.body
+        console.log(req.socket.remoteAddress)
+
+
+
+        let data = fs.readFileSync("./users.json", "utf8");
+        let js = JSON.parse(data);
+        let device = js[id][0]
+        const now = new Date
+        get_time = now.toLocaleString()
+        console.log(now.toLocaleString())
+        device.time_updated = get_time
+        device.value = value
+        console.log(value)
+        js[id][0] = device
+        fs.writeFileSync("./users.json", JSON.stringify(js));
+        res.send('123')
+
+    }
+    else{
+        res.status(400).send("Failed to change")
+    }
+
+    
+});
+
+
+
+
+
+
 app.listen(process.env.PORT || 3000)
