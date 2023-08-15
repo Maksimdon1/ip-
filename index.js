cc3e1c7e74727779d40b0011086dc65a7efba5b9
Maksimdon1 <111438708+Maksimdon1@users.noreply.github.com>
2023-08-15 19:52:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     console.log(req.socket.remoteAddress)
+    console.log(req.headers['x-forwarded-for'] )
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
